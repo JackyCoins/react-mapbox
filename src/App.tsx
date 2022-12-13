27763a2e1493177d02871160a981564fd528e241
Nikita Rusanov <n.rusanov@nicity.com>
2022-12-13 13:57:00 +0800

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,28 @@ import "./App.css";
 import { SimpleMap } from "./examples";
 import { MapDIContextProvider } from "./map/MapDIContext";
 
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        Hello world!<Link to={"/simple"}>Click</Link>
+      </div>
+    ),
+  },
+  {
+    path: "/simple",
+    element: <SimpleMap />,
+  },
+]);
+
 function App() {
   return (
     <MapDIContextProvider>
       <div className="App">
-        <SimpleMap />
+        <RouterProvider router={router} />
       </div>
     </MapDIContextProvider>
   );
--- src/examples/SimpleMap.tsx
+++ src/examples/SimpleMap.tsx
@@ -1,8 +1,11 @@
 import { BaseMap } from "../map/BaseMap";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const SimpleMap = () => {
   return (
     <div>
+      <Link to={"/"}>Click</Link>
       <BaseMap name={"simple"} />
     </div>
   );
